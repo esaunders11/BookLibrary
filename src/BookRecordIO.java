c1ae44942d43344d009737bcf74c71b1ceb21931
essaund2 <essaund2@ncsu.edu>
2024-09-20 18:13:44 -0400

    Implement Book,Genre,BookRecordIO

---
+++ src/BookRecordIO.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.PrintStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+/**
+ * Class that reads and write Library files
+ * @author Ethan Saunders
+ */
+public class BookRecordIO {
+    
+    /**
+     * Reads in ArrayList of Books from a File
+     * @param filename name of file
+     * @return ArrayList of Books
+     * @throws FileNotFoundException if file is not found
+     */
+    public static ArrayList<Book> readCourseRecords(String filename) throws FileNotFoundException {
+        Scanner scan = new Scanner(new FileInputStream(filename));
+        ArrayList<Book> books = new ArrayList<Book>();
+        
+        while (scan.hasNextLine()) {
+            try {
+                Book book = readBook(scan.nextLine());
+
+                boolean duplicate = false;
+
+                for (int i = 0; i < books.size(); i++) {
+                    Book current = books.get(i);
+
+                    if (current.equals(book)) {
+                        duplicate =  true;
+                        break;
+                    }
+                }
+
+                if (!duplicate) {
+                    books.add(book);
+                }
+
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+        
+        }
+        scan.close();
+        return books;
+    }
+
+    /**
+     * Creates a Book object from a given line
+     * @param line line in a file
+     * @return Book object
+     */
+    private static Book readBook(String line) {
+        Scanner scan = new Scanner(line);
+        scan.useDelimiter(",");
+        
+        try {
+            String title = scan.next();
+            String author = scan.next();
+            int length = scan.nextInt();
+
+            if (scan.hasNext()) {
+                scan.close();
+                throw new IllegalArgumentException("Invalid file.");
+            }
+            else {
+                scan.close();
+                return new Book(title, author, length);
+            }
+        } catch (Exception e) {
+            scan.close();
+            throw new IllegalArgumentException("Invalid file.");
+        }
+    }
+
+    /**
+     * Writes a file of the libraries contents
+     * @param filename name of file to write in 
+     * @param books ArrayList of Books
+     * @throws IOException if unable to write to file
+     */
+    public static void writeBookRecors(String filename, ArrayList<Book> books) throws IOException {
+        PrintStream writer = new PrintStream(new File(filename));
+
+        for (Book b : books) {
+            writer.println(b.toString());
+        }
+
+        writer.close();
+    }
+}
---
+++ src/Genre.java
@@ -0,0 +1,44 @@
+/**
+ * Genre class
+ * 
+ * @author Ethan Saunders
+ */
+public class Genre {
+    /** List of available genres */
+    public static final String[] genres = {"Fiction", "Non-Fiction", "Romance", "Mystery", "Fantasy", "Sci-Fi", "Historical Fiction",
+    "Western", "Biography", "Memior", "Children", "Horror"};
+    /** Type of genre */
+    private String type;
+
+    /**
+     * Creates a genre object
+     * @param type Type of genre.
+     */
+    public Genre(String type) {
+        setType(type);
+    }
+
+    /**
+     * Sets the type of genre to given genre only if genre is in available list
+     * @param type Type of genre
+     * @throws IllegalArgumentException if genre is not available
+     */
+    public void setType(String type) {
+        for (String g : genres) {
+            if (type.equals(g)){
+                this.type = type;
+            }
+        }
+        if (this.type == null || "".equals(type)) {
+            throw new IllegalArgumentException("Invalid Genre.");
+        }
+    }
+
+    /**
+     * Returns objects type
+     * @return Genre String
+     */
+    public String getType() {
+        return this.type;
+    }
+}
